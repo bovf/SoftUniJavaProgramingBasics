9ce11a11f98c2bce9b9c1d55f15307ab765f4440
Dobry Nikolov <dobry989@gmail.com>
2021-04-09 17:37:13 +0300

    Solved the first problem and pushed it.

---
+++ src/ConditionalStatements/PB1ExcellentResult.java
@@ -0,0 +1,17 @@
+package ConditionalStatements;
+
+import java.util.Scanner;
+
+public class PB1ExcellentResult {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        int grade = Integer.parseInt(scanner.nextLine());
+
+        if (grade >= 5) {
+            System.out.println("Excellent!");
+        }else {
+            ;
+        }
+
+    }
+}
