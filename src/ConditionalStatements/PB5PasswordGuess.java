c96c1f4698ca64cfc0c0a5bd69bb4ccd88e3227b
Dobry Nikolov <dobry989@gmail.com>
2021-04-11 17:53:19 +0300

    Solved the fifth problem and pushed it.

---
+++ src/ConditionalStatements/PB5PasswordGuess.java
@@ -0,0 +1,19 @@
+package ConditionalStatements;
+
+import java.util.Scanner;
+
+public class PB5PasswordGuess {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String password = scanner.nextLine();
+
+        String realPassword = "s3cr3t!P@ssw0rd";
+
+        if (password.equals(realPassword)){
+            System.out.println("Welcome");
+        }else{
+            System.out.println("Wrong password!");
+        }
+    }
+}
