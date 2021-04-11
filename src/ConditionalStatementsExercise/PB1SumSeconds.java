194b09efadeaaab5be82cd8a5d7872ce31b4d469
Dobry Nikolov <dobry989@gmail.com>
2021-04-11 18:20:10 +0300

    Started to solve the first problem but Bill Burr distracted me...

---
+++ src/ConditionalStatementsExercise/PB1SumSeconds.java
@@ -0,0 +1,11 @@
+package ConditionalStatementsExercise;
+
+import java.util.Scanner;
+
+public class PB1SumSeconds {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
