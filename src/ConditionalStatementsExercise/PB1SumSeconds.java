be460b4c25bb20c744c276bcc9ffe61195b74c9c
Dobry Nikolov <dobry989@gmail.com>
2021-04-13 00:57:20 +0300

    Solved the first problem and pushed it.

--- src/ConditionalStatementsExercise/PB1SumSeconds.java
+++ src/ConditionalStatementsExercise/PB1SumSeconds.java
@@ -7,5 +7,27 @@ public class PB1SumSeconds {
 
         Scanner scanner = new Scanner(System.in);
 
+        int firstFinish = Integer.parseInt(scanner.nextLine());
+        int secondFinish = Integer.parseInt(scanner.nextLine());
+        int thirdFinish = Integer.parseInt(scanner.nextLine());
+
+        int totalSeconds = firstFinish + secondFinish + thirdFinish;
+        double totalMinutes = Math.floor((double) totalSeconds / 60);
+        double seconds = totalSeconds % 60;
+
+        if (totalMinutes < 10) {
+            if (seconds < 10) {
+                System.out.printf("%.0f:0%.0f", totalMinutes, seconds);
+            } else {
+                System.out.printf("%.0f:%.0f", totalMinutes, seconds);
+            }
+        } else {
+            if (seconds < 10) {
+                System.out.printf("%.0f:0%.0f", totalMinutes, seconds);
+            } else {
+                System.out.printf("%.0f:%.0f", totalMinutes, seconds);
+            }
+
+        }
     }
 }
