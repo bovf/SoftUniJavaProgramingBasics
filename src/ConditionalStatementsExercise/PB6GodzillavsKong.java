cda1b7905840b7c34fb0138368351090e472ecc0
Dobry <dobry989@gmail.com>
2021-12-07 21:59:13 +0200

    Something idk :)

---
+++ src/ConditionalStatementsExercise/PB6GodzillavsKong.java
@@ -0,0 +1,12 @@
+package ConditionalStatementsExercise;
+
+import java.util.Scanner;
+
+public class PB6GodzillavsKong {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
